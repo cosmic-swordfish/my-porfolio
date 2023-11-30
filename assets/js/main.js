ec97d7bdf3d398f18c9f037a4bf201d7a6c7d704
Ruben Shahi <reubenshahi2015@gmail.com>
2023-11-30 18:32:16 +0545

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,6 +89,40 @@
     AOS.init({
       duration: 1000,
       easing: "ease-in-out",
+
+
+
+
+
+        const span = (text, index) => {
+  const node = document.createElement('span')
+
+  node.textContent = text
+  node.style.setProperty('--index', index)
+  
+  return node
+}
+
+export const byWord = text =>
+  text.split(' ').map(span)
+
+const {matches:motionOK} = window.matchMedia(
+  '(prefers-reduced-motion: no-preference)'
+)
+
+if (motionOK) {
+  const splitTargets = document.querySelectorAll('[split-by]')
+
+  splitTargets.forEach(node => {
+    let nodes = byWord(node.innerText)
+
+    if (nodes)
+      node.firstChild.replaceWith(...nodes)
+  })
+}
+        
+
+      
       once: true,
       mirror: false,
     });
